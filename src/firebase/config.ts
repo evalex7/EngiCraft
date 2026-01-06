46ba3520478d317d90f68fdf9aa3e810709ff9cc
evalex7 <evalex.work@gmail.com>
2026-01-06 11:45:40 +0200

    Add storageBucket to Firebase config
    Added storageBucket to Firebase configuration.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,6 @@ export const firebaseConfig = {
   "appId": "1:374173524082:web:6bbd3c2da45be649fb62eb",
   "apiKey": "AIzaSyAf6BwqwejYoNFgCJzesilTJgT1AU7mun4",
   "authDomain": "studio-1701377444-def46.firebaseapp.com",
-  "measurementId": "",
+  "storageBucket": "studio-1701377444-def46.appspot.com",
   "messagingSenderId": "374173524082"
 };
